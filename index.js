32099194d3ded9efb27dac875ddbd5b9d4d15460
Samanthaponce5 <samanthaponce5@gmail.com>
2020-04-06 12:35:16 -0400

    Done.

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ function moveDodgerLeft() {
 	let left = parseInt(leftNumber, 10);
 
     if (left > 0) 
-   dodger.style.left = `${left - 1}px`
+   dodger.style.left = `${left - 15}px`
 }
 
 document.addEventListener('keydown', function(e){
     if (e.key === 'ArrowLeft' )
    moveDodgerLeft()
+})
+
+function moveDodgerRight(){
+    let rightNumber = dodger.style.left.replace('px', '')
+    let right = parseInt(rightNumber, 10)
+
+    if (right < 360)
+    dodger.style.left = `${right + 15}px`
+}
+document.addEventListener('keydown', function(e){
+    if (e.key === 'ArrowRight')
+    moveDodgerRight()
 })
